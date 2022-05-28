3c872d12fa2ef0bf36ab9737c930414af69f39db
natserract <benjaminstwo@gmail.com>
2022-05-29 00:35:23 +0800

    feat: example of default, nested ways

--- examples/examples.js
+++ examples/examples.js
@@ -1,10 +1,33 @@
 const { predict } = require('../dist/lib')
 
+// :: Default way
 predict(true).execute({
   if() {
     console.log(true)
   },
   else() {
     console.log(false)
+  }
+})
+
+
+// :: Nested way
+//
+const text = "hello world"
+
+predict(text.length > 3).execute({
+  if(thenIf){
+    thenIf(text.length < 5).execute({
+      if() {
+        console.log(true)
+      },
+      else(thenElse) {
+        thenElse(text.split('')[0] == "h").execute({
+          if() {
+            console.log(false)
+          }
+        })
+      }
+    })
   }
 })
