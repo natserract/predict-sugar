fe11aa00f4f99ec5fe302528fc42f7571dadb91e
natserract <benjaminstwo@gmail.com>
2022-05-29 12:41:36 +0800

    fix: incorrect path types declarations

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,4 @@
 import Predict from './lib/predict'
-import {
-  OverrideFunctionStatement,
-  ReturnOfStatement,
-  StatementArg,
-  StatementCollection,
-  StatementFunction
-} from './types/types'
-
-export {
-  StatementArg,
-  StatementFunction,
-  ReturnOfStatement,
-  StatementCollection,
-  OverrideFunctionStatement as OverrideStatementFunction
-}
 
 const { predict, predictAnd, predictOr } = new Predict()
 export { predict, predictAnd, predictOr }
